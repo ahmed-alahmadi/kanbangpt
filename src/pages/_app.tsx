6a3479c5e006a577b711885879564b6cc363515b
Ahmed Alahmadi <ahmedd.alahmadi@gmail.com>
2023-08-30 20:52:13 +0300

    add planetscale

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,13 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+import { ClerkProvider } from "@clerk/nextjs";
+import type { AppProps } from "next/app";
+const MyApp: AppType = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
-
 export default api.withTRPC(MyApp);
