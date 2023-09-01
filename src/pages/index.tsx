f8c4e25d4f41d7240e6b5573adbd5d9f8f636451
Ahmed Alahmadi <ahmedd.alahmadi@gmail.com>
2023-09-01 22:40:37 +0300

    Move components folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Link from "next/link";
 
 import { api } from "~/utils/api";
-import { NavBar } from "./components/NavBar";
+import { NavBar } from "../components/NavBar";
 
 export default function Home() {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
